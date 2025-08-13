a6b3aa825671c94e39398c163987778366bc0e4e
Wesllen <wesllenaraujo.developer@gmail.com>
2025-08-13 20:33:05 -0300

    Some styles in style.css file was adjusted. Created a responsive web design for screens of width lower than  960px. Added a function of the hamburger bar with JavaScript.

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,32 @@
-let button = document.getElementById('mainButton');
-let arrowIcon = document.querySelector('i');
-button.addEventListener('mouseover', function() {
-    arrowIcon.style.color = 'var(--textDarkColor)';
-})
-button.addEventListener('mouseleave', function() {
-    arrowIcon.style.color = 'var(--textColor)';
-})
+let button = document.getElementById("mainButton");
+let arrowIcon = document.querySelector("i");
+button.addEventListener("mouseover", function () {
+  arrowIcon.style.color = "var(--textDarkColor)";
+});
+button.addEventListener("mouseleave", function () {
+  arrowIcon.style.color = "var(--textColor)";
+});
 
 function reg() {
-    window.captureEvents(Event.SUBMIT);
-    window.onsubmit = hit;
+  window.captureEvents(Event.SUBMIT);
+  window.onsubmit = hit;
 }
 function hit() {
-    let notification = document.getElementById('notificationSection'); 
-    notification.style.display = 'flex';
-    setTimeout(() => {
-        notification.style.display = 'none';
-    }, 5000);
-  }
+  let notification = document.getElementById("notificationSection");
+  notification.style.display = "flex";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 5000);
+}
+const barMenu = document.querySelector(".barMenu");
+const menu = document.querySelector(".menu")
+barMenu.addEventListener("click", (e) => {
+    if (menu.style.display == "none") {
+        menu.style.display = "flex";
+    } else {
+        menu.style.display = "none";
+    }
+})
+window.onresize = function () {
+  menu.style.display = "flex";
+};
