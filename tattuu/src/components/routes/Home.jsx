64f4f47f8077eed5e46e6c9c0e85c98ac6917189
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-22 09:35:23 -0700

    Create styles of Hero section from Home page.

--- tattuu/src/components/routes/Home.jsx
+++ tattuu/src/components/routes/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div className="bg-custom-dark-purple">
       <div className="w-full h-screen bg-desktop-hero mask-t-from-90% mask-b-from-80% flex justify-end">
         <div className="mr-8 gap-8 w-2/5 flex flex-col self-center">
-          <h1 className="text-6xl mr-8 font-courgette font-bold">
-            Tatuagens Customizadas em Porto Alegre - RS
+          <h1 className="text-5xl mr-50 leading-14 font-courgette font-bold">
+            Tatuagens Customizadas & Fotografias Profissionais em Porto Alegre - RS
           </h1>
           <div className="w-3/4 flex flex-col gap-6 items-center self-center">
             <h2 className="text-3xl font-courgette">Transforme-se em uma arte ambulante.</h2>
